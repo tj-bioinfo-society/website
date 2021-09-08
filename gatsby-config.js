148260e88dfa9000212ee7e136637019684f2abc
Sritan Motati <sritanmotati@gmail.com>
2021-09-08 15:53:38 -0400

    manifest error

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "src/images/logo.png",
       },
     },
     "gatsby-plugin-sharp",
