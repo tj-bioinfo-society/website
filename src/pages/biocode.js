2143fdcc71edef59fe275509d07a281a0173eebf
Sritan Motati <sritanmotati@gmail.com>
2022-03-12 20:50:34 -0500

    added biocode link

--- src/pages/biocode.js
+++ src/pages/biocode.js
@@ -21,6 +21,23 @@ const BiocodePage = () => {
     <Layout>
       <SEO title="BioCode" description="The BioCode competition is a prorgramming competition held by TJ Bioinformatics Society that challenges students to solve biology-related problems efficiently using code." />
       <Hero image={heroImage} title={"TJHSST BioCode"} subtitle={"A bioinformatics competition for high school students we host where students must solve biology-related problems efficiently using code."} centered={true} />
+      <div className='bg-gray-100'>
+        <div className='max-w-screen-lg mx-auto py-10 px-4 sm:px-6'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 gap-5'>
+            <div className='text-center sm:text-left'>
+              <h1 className="text-2xl tracking-tight leading-8 sm:leading-none font-extrabold sm:text-3xl lg:text-4xl xl:text-5xl mb-4">
+                Sign up now!
+              </h1>
+              <p className='text-md text-gray-500'>
+                Fill out the registration form to register for TJ BioCode, where you can learn new skills and potentially win hundreds of dollars in prizes!
+              </p>
+            </div>
+            <div className='flex justify-center sm:justify-end items-center sm:text-right'>
+              <a href="https://forms.gle/zvxHHpE37jg8BJBBA" target="_blank" className="bg-green-500 hover:bg-green-400 focus:outline-none focus:bg-green-400 px-5 py-3 border border-transparent text-base font-medium rounded-md text-white transition duration-150 ease-in-out">Register</a>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className='mx-auto max-w-screen-lg py-20 pt-20 px-4 sm:px-8 md:px-12'>
         <div className='mb-8'>
           <p className="text-gray-500 text-lg">
