76122407fb438c3b63b269f81958e102e4eb0198
Sritan Motati <sritanmotati@gmail.com>
2022-09-08 14:56:15 -0400

    added lectures section for new year

--- src/pages/lectures.js
+++ src/pages/lectures.js
@@ -1,5 +1,6 @@
 import { graphql, useStaticQuery } from "gatsby"
 import * as React from "react"
+import { useState } from 'react'
 import Hero from "../components/hero"
 import Layout from "../components/layout"
 import LectureData from '../data/lectures.json'
@@ -17,12 +18,22 @@ const LecturesPage = () => {
 			}
     }
   `)
+  const years = ['2021-22', '2022-23'].reverse()
+  const [curYear,setCurYear] = useState(years[0])
+
   return (
     <Layout>
       <SEO title="Lectures" description="Past lectures from TJ Bioinformatics Society covering a wide range of topics in bioinformatics." />
       <Hero image={heroImage} title={"Lectures"} />
       <div className='mx-auto max-w-screen-lg py-20 pt-20 px-4 sm:px-8 md:px-12'>
-        <h1 className='font-extrabold text-3xl mb-6'>2021-22 Lectures</h1>
+        <h1 className='font-extrabold text-3xl mb-6'>{years[0]} Lectures</h1>
+        <div className='flex space-x-6 justify-center flex-wrap'>
+          {years.map(year => {
+            return (
+              <button className={"font-bold px-8 py-4 mb-6 text-black rounded-md shadow " + (year == curYear ? "bg-green-100 hover:bg-green-100" : "bg-gray-100 hover:bg-gray-200")} onClick={() => setCurYear(year)}>{year}</button>
+            )
+          })}
+        </div>
         <div className="overflow-scroll max-w-screen-lg no-scrollbar">
           <table className="w-full bg-transparent mx-auto rounded-md" style={{minWidth: "500px"}}>
             <thead>
@@ -33,13 +44,13 @@ const LecturesPage = () => {
               </tr>
             </thead>
             <tbody>
-              {LectureData["2021"].map(lecture => {
+              {LectureData[curYear].map(lecture => {
                 return (
                   <tr className='hover:bg-gray-100'>
                     <td className='border-b-2 py-3 px-4'>{lecture.date}</td>
                     <td className='border-b-2 py-3 px-4'>
                       {lecture.url ? (
-                        <a className='text-green-500 font-semibold' href={lecture.url} target="_blank">{lecture.title}</a>
+                        <a className='text-green-500 font-semibold' href={lecture.url} target={lecture.url ? "_blank" : "#"}>{lecture.title}</a>
                       ) : (
                         <p className='text-green-500 font-semibold cursor-pointer'>{lecture.title}</p>
                       )}
