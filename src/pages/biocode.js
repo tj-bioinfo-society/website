b745216c4595a1188bf5c9e7fffa5318ebbbdced
Sritan Motati <sritanmotati@gmail.com>
2023-02-15 15:47:53 -0500

    updated biocode page

--- src/pages/biocode.js
+++ src/pages/biocode.js
@@ -43,51 +43,46 @@ const BiocodePage = () => {
           <a href="https://docs.google.com/document/d/1AA084FChANFJe_CPFmHl207dfgHzoUS-pI0ldBFrFxU/edit?usp=sharing" target="_blank" className="bg-green-500 hover:bg-green-400 focus:outline-none focus:bg-green-400 px-6 py-4 border border-transparent text-lg font-medium rounded-md text-white transition duration-150 ease-in-out">Welcome Packet</a>
         </div> */}
         <div className='mb-8'>
+          <p className="text-gray-500 text-lg mb-3">
+          TJHSST BioCode is an annual coding competition hosted by the Thomas Jefferson High School 
+          for Science and Technology Bioinformatics Society. At BioCode, teams can participate in 1 of 2
+          categories: 1) an algorithmic competitive programming style competition to solve the most 
+          number of problems, within a wide range of difficulty (these problems are all related to 
+          biology but require no previous biological knowledge and only minimal CS experience), or 2) 
+          a project-based hackathon where students create and submit any bioinformatics-driven program 
+          or style. Participation in BioCode is completely free, and we have more than <strong>$1,500 in prizes</strong>. 
+          The competition will consist of an opening ceremony, programming time, workshops from 
+          knowledgeable TJ students, guest lectures from professors in the field, and an awards ceremony.
+          </p>
+          <p className="text-gray-500 text-lg mb-1">
+          Where: Thomas Jefferson High School for Science and Technology, 6560 Braddock Road, Alexandria, VA 22312  
+          </p>
           <p className="text-gray-500 text-lg">
-            TJHSST BioCode is an annual coding competition hosted by TJ Bioinformatics Society. At BioCode,
-            teams compete with each other to solve the most number of problems, within a wide range of difficulty. 
-            hese problems are all related to biology but require no previous biological knowledge and only minimal CS experience. 
-            The competition is completely free and we have free food and over $1500 in prizes. The competition will consist of an 
-            opening ceremony, 4 hours of programming time, and an awards ceremony.
+          When: March 11, 10 AM - 7 PM EST
           </p>
         </div>
         <div className='mb-8'>
           <h1 className='font-extrabold text-4xl mb-4'>Structure</h1>
           <p className="text-gray-500 text-lg mb-3">
-            There will be three divisions: middle school, basic high school (have not taken AP 
-            Computer Science), and advanced high school (have taken AP Computer Science). Prizes 
-            will be awarded to the top 10 middle school teams, and top 3 high school teams in each 
-            category. If you currently do not have a team, we will pair you with other students in 
-            your division. More information will be given during opening ceremonies.
+          For each competition category (competitive programming or hackathon), there will be three divisions: beginner (ex; middle schoolers), intermediate (ex: have not taken AP Computer Science, in high school), and advanced (have taken AP Computer Science). Prizes will be awarded to the top 3 teams in each division, for each category. If you currently do not have a team, we will pair you with other students in your division if you’d like. More information will be released soon, and given during opening ceremonies.
+          </p>
+          <p className="text-gray-500 text-lg mb-3">
+          For competitive programming, teams of up to four will receive problems to complete (anything as simple as "translate this DNA into RNA" to "model equilibrium in this population"). Problems will cover all the biology you need - essentially, you will solve coding problems that happen to be about biology. Problems are designed so even with simple knowledge of Java/Python/C++/other languages, you will be able to do a significant amount of the problems. Descriptions of the programming problems, test cases, and point values are given in a packet. More information about problem submission will be given during opening ceremonies.
           </p>
           <p className="text-gray-500 text-lg">
-            During the programming portion, teams of up to four will receive a set of problems 
-            to complete (anything as simple as "translate this DNA into RNA" to "model equilibrium 
-            in this population"). The problems will explain all the biology you need to solve the 
-            problem - essentially, you will solve coding problems that happen to be about biology. 
-            We designed the problem set so that, even with simple knowledge of Java/Python/C++/other 
-            languages, you will be able to do a significant amount of the problems. More difficult 
-            problems will be worth more points. Descriptions of the programming problems, test cases, 
-            and point values are given in a packet. More information about problem submission will be 
-            given during opening ceremonies.
+          For the hackathon, an overview direction/focus will be presented during the opening ceremony, and teams of up to 4 can design any program (ex; website, mobile app, machine learning model) which uses bioinformatics techniques, in the context of the focus. As an example, the focus might look like: How can computer science be used to help diseased people?  
           </p>
         </div>
         <div className='mb-8'>
           <h1 className='font-extrabold text-4xl mb-4'>Workshops</h1>
           <p className="text-gray-500 text-lg">
-            In addition, if you finish early, cannot solve any more problems, or are interested in 
-            learning something new, then there are sessions during those 4 hours of programming where 
-            you can learn more about bioinformatics, programming, or the school TJHSST. In these 
-            lectures, you can learn a new programming language, get assistance with solving one of 
-            the competition problems, or learn about how bioinformatics is applied in the real world. 
-            More information about these sessions will be given during opening ceremonies.
+          If you finish early, cannot solve any more problems, or are interested in learning something new, BioCode will have numerous workshops hosted by knowledgeable TJ students. In these lectures and activities, you can learn a new programming language, get assistance with solving one of the competition problems, or learn about how bioinformatics is applied in the real world. More information about these sessions will be given during opening ceremonies.
           </p>
         </div>
         <div className='mb-8'>
           <h1 className='font-extrabold text-4xl mb-4'>Prizes</h1>
           <p className="text-gray-500 text-lg">
-            Each division will have individual prizes for the winners of their respective divisions. 
-            These prizes will include a cash prize and possible sponsored prizes (to be announced later).
+          Each division will have individual prizes for the winners of their respective divisions. These prizes will include a cash prize and possible sponsored prizes (to be announced later).
           </p>
         </div>
         <div>
