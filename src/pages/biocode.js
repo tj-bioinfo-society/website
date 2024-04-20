04dd8886c569345f81e9a7cc826ce3af5d9e6e9c
Sritan Motati <sritanmotati@gmail.com>
2024-04-20 09:39:53 -0400

    added pg button

--- src/pages/biocode.js
+++ src/pages/biocode.js
@@ -40,9 +40,9 @@ const BiocodePage = () => {
         </div>
       </div>
       <div className='mx-auto max-w-screen-lg py-20 pt-16 px-4 sm:px-8 md:px-12'>
-        {/* <div className="mb-12 text-center">
-          <a href="https://drive.google.com/file/d/18b4crGDsIi5bvROAng0OeOdlpsUOIBq-/view?usp=sharing" target="_blank" className="bg-green-500 hover:bg-green-400 focus:outline-none focus:bg-green-400 px-6 py-4 border border-transparent text-lg font-medium rounded-md text-white transition duration-150 ease-in-out">Participant Guide</a>
-        </div> */}
+        <div className="mb-12 text-center">
+          <a href="https://docs.google.com/document/d/19cQ1VvRE0Z9xtxzWR5XcVMYZkPIIVlFh9izFqw-mGTA/edit?usp=sharing" target="_blank" className="bg-green-500 hover:bg-green-400 focus:outline-none focus:bg-green-400 px-6 py-4 border border-transparent text-lg font-medium rounded-md text-white transition duration-150 ease-in-out">Participant Guide</a>
+        </div>
         <div className='mb-8'>
           <p className="text-gray-500 text-lg mb-3">
           TJHSST BioCode is an annual coding competition hosted by the Thomas Jefferson High School 
